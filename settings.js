8c4c9bfd24b57cc98b6ea6612892265d9a68ad41
BOY-INNOCENT <anepatiyooo@gmail.com>
2025-03-26 14:47:13 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: '◦◦기욷❂𝐏𝐀𝐍𝐃𝐀│-̲̅-̲̅-̲̅-̲̅❂➣', // Your name
+  ownerNumber: '94754070649', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
